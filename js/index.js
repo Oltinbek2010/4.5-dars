4b55bcc6cc23cea67ee18b29e57a1393de06165b
Oltinbek2010 <oltinbek04@gmail.com>
2024-06-08 18:04:36 +0500

    commit

---
+++ js/index.js
@@ -0,0 +1,514 @@
+// BREAK/CONTINUE
+
+// 1-MASALA
+
+// let i = 0;
+// while (i <= 50) {
+//     if (i == 30) {
+//         break;
+//     }
+//     console.log(i);
+//     i++;
+// }
+
+// 2-masala
+
+// for (let i = 1; i <= 50; i++) {
+//     if (i % 5 == 0) continue;
+//     console.log(i);
+// }
+
+// 3-masala
+
+// for (let i = 1; i <= 100; i++) {
+//     if (i == 75) {
+//         break; 
+//     }
+//     console.log(i);
+// }
+
+// 4-masala
+
+// for (let i = 1; i <= 30; i++) {
+//     if (i % 3 === 0) {
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+// WHILE/ DO ... WHILE
+
+// while
+
+// 1-masala
+
+// let i = 1
+// while (i <= 20) {
+//     console.log(i);
+//     i++;
+// }
+
+// 2-masala
+
+// let i = 10;
+//  while (i >= 1) {
+//    console.log(i);
+//    i--;
+// };
+
+// 3-masala
+
+// let i = 1;
+
+// while (i <= 50) {
+//   if (i % 2 == 0) {
+//      console.log(i);
+//   }
+//   i++;
+// };
+
+// 4-masala
+
+// let i = 1;
+
+// while (i <= 50) {
+//     console.log(i);
+//     i += 2;
+// }
+
+// 5-masala
+
+// let i = 1;
+// let sum = 0;
+
+// while (i <= 100){
+// sum += i;
+//     i++;
+// };
+// console.log(sum);
+
+// 6-masala
+// let i = 1;
+// while (i <= 10) {
+//     let kvadrat = i * i;
+//     console.log("Son: " + i + ", Kvadrati: " + kvadrat);
+//     i++;
+// }
+
+// 7-masala
+
+// TUSHUNMADIM
+
+// 8-masala
+
+// TUSHUNMADIM
+
+// 9-masala
+
+// TUSHUNMADIM
+
+// 10-masala
+
+// let i = 1;
+
+// while (i <= 20) {
+//     if (i % 3 == 0) {
+//         console.log(i);
+//     }
+//     i++;
+// }
+
+// do ... while
+
+// 11-masala
+
+// let i = 1;
+
+// do {
+//     console.log(i);
+//     i++;
+// } while (i <= 10);
+
+// 12-masala
+
+// let i = 20;
+
+// do {
+//     console.log(i);
+//     i--;
+// } while (i >= 1);
+
+// 13-masala
+
+// let i = 1;
+
+// do {
+//     if (i % 2 == 0) {
+//         console.log(i);
+//     }
+//     i++;
+// } while (i <= 100);
+
+// 14-masala
+
+// let n = +prompt(`Son kiriting`);
+// do {
+//     console.log(n**2);
+//     n--;
+// } while (n >= 0);
+
+// 15-masala
+
+// let n = +prompt(`Son kiriting`);
+
+// do {
+//     console.log(n);
+//     n++;
+// } while (n <= 10);
+
+// UMUMIY MASALALAR
+// 1-masala
+// let result;
+// for (let i = 1; i <= 50; i++) {
+//     if (i == 35) {
+//         console.log(`besh va yettiga karrali karrali`);
+//     }else if (i % 5== 0) {
+//         console.log(`beshga karrali`);
+//     }else if (i % 7== 0) {
+//         console.log(`yettiga karrali`);
+//     }else {
+//         console.log(i);
+//     }
+// }
+
+//2-masala
+// let a = 1;
+// let b = 2;
+// let c = 3;
+// let d = 4;
+// let e = 5;
+// let f = 6;
+// let g = 7;
+// let h = 8;
+// let i = 9;
+// let j = 10;
+// let k = 11;
+// let l = 12;
+// switch (a) {
+//     case 1:
+//         console.log(`1 - "Yanvar"`);
+//         break;
+
+//     default:
+//         console.log(ERROR);
+//         break;
+// }
+// switch (b) {
+//     case 2:
+//         console.log(`2 - "Fevral"`);
+//         break;
+
+//     default:
+//         console.log(ERROR);
+//         break;
+// }
+// switch (c) {
+//     case 3:
+//         console.log(`3 - "Mart"`);
+//         break;
+
+//     default:
+//         console.log(ERROR);
+//         break;
+// }
+// switch (d) {
+//     case 4:
+//         console.log(`4 - "Aprel"`);
+//         break;
+
+//     default:
+//         console.log(ERROR);
+//         break;
+// }
+// switch (e) {
+//     case 5:
+//         console.log(`5 - "May"`);
+//         break;
+
+//     default:
+//         console.log(ERROR);
+//         break;
+// }
+// switch (f) {
+//     case 6:
+//         console.log(`6 - "Iyun"`);
+//         break;
+
+//     default:
+//         console.log(ERROR);
+//         break;
+// }
+// switch (g) {
+//     case 7:
+//         console.log(`7 - "Iyul"`);
+//         break;
+
+//     default:
+//         console.log(ERROR);
+//         break;
+// }
+// switch (h) {
+//     case 8:
+//         console.log(`8 - "Avgust"`);
+//         break;
+
+//     default:
+//         console.log(ERROR);
+//         break;
+// }
+// switch (i) {
+//     case 9:
+//         console.log(`9 - "Sentyabr"`);
+//         break;
+
+//     default:
+//         console.log(ERROR);
+//         break;
+// }
+// switch (j) {
+//     case 10:
+//         console.log(`10 - "Oktyabr"`);
+//         break;
+
+//     default:
+//         console.log(ERROR);
+//         break;
+// }
+// switch (k) {
+//     case 11:
+//         console.log(`11 - "Noyabr"`);
+//         break;
+
+//     default:
+//         console.log(ERROR);
+//         break;
+// }
+// switch (l) {
+//     case 12:
+//         console.log(`12 - "Dekabr"`);
+//         break;
+
+//     default:
+//         console.log(ERROR);
+//         break;
+// }
+
+// 3-masala
+
+// let i = 1;
+// while (i <= 100) {
+//     if (i % 2 == 0) {
+//         continue;
+//     }else if (i == 77) {
+//         break;
+//     }
+//     console.log(i);
+//     i++;
+// }
+
+// 4-masala
+
+// let a = 1;
+// let b = 2;
+// let c = 3;
+// let d = 4;
+// let e = 5;
+// let f = 6;
+// let g = 7;
+
+// switch (a) {
+//     case 1:
+//         console.log(`1 - "Dushanba"`);
+//         break;
+
+//     default:
+//         console.log(ERROR);
+//         break;
+// }
+// switch (b) {
+//     case 2:
+//         console.log(`2 - "Seshanba"`);
+//         break;
+
+//     default:
+//         console.log(ERROR);
+//         break;
+// }
+// switch (c) {
+//     case 3:
+//         console.log(`3 - "Chorshanba"`);
+//         break;
+
+//     default:
+//         console.log(ERROR);
+//         break;
+// }
+// switch (d) {
+//     case 4:
+//         console.log(`4 - "Payshanba"`);
+//         break;
+
+//     default:
+//         console.log(ERROR);
+//         break;
+// }
+// switch (e) {
+//     case 5:
+//         console.log(`5 - "Juma"`);
+//         break;
+
+//     default:
+//         console.log(ERROR);
+//         break;
+// }
+// switch (f) {
+//     case 6:
+//         console.log(`6 - "Shanba"`);
+//         break;
+
+//     default:
+//         console.log(ERROR);
+//         break;
+// }
+// switch (g) {
+//     case 7:
+//         console.log(`7 - "Yakshanba"`);
+//         break;
+
+//     default:
+//         console.log(ERROR);
+//         break;
+// }
+
+// 5-masala
+
+// let i = +prompt (`Son kiriting`);
+
+// do {
+//     if (i > 0) {
+//         console.log(`Musbat`);
+//         i--;
+//     }else if(i < 0) {
+//         console.log(`Manfiy`);
+//         i++;
+//     }else if (i == 0) {
+//         console.log(`Nol`);
+//         break;
+//     } 
+    
+// } while (i > 0);
+
+// 6-masala
+
+// let a = +prompt(`Raqam kiriting`);
+
+
+// switch (a) {
+//     case 1:
+//         console.log(`1 - "Dushanba"`);
+//         break;
+
+//     default:
+//         console.log(ERROR);
+//         break;
+// }
+// switch (a) {
+//     case 2:
+//         console.log(`2 - "Seshanba"`);
+//         break;
+
+//     default:
+//         console.log(ERROR);
+//         break;
+// }
+// switch (a) {
+//     case 3:
+//         console.log(`3 - "Chorshanba"`);
+//         break;
+
+//     default:
+//         console.log(ERROR);
+//         break;
+// }
+// switch (a) {
+//     case 4:
+//         console.log(`4 - "Payshanba"`);
+//         break;
+
+//     default:
+//         console.log(ERROR);
+//         break;
+// }
+// switch (a) {
+//     case 5:
+//         console.log(`5 - "Juma"`);
+//         break;
+
+//     default:
+//         console.log(ERROR);
+//         break;
+// }
+// switch (a) {
+//     case 6:
+//         console.log(`6 - "Shanba"`);
+//         break;
+
+//     default:
+//         console.log(ERROR);
+//         break;
+// }
+// switch (a) {
+//     case 7:
+//         console.log(`7 - "Yakshanba"`);
+//         break;
+
+//     default:
+//         console.log(ERROR);
+//         break;
+// }
+
+// 7-masala
+
+// for (let i = 1; i < 100; i++) {
+//     if (i % 3 == 0 && i % 5 == 0) {
+//         console.log('FizzBuzz');
+//     }
+//     else if (i % 3) {
+//         console.log('Fizz');
+//     }else if (i % 5) {
+//         console.log('Buzz');
+//     }
+//     else{
+//         console.log(i);
+//     }
+// }
+
+// 8-masala
+
+// let son = +prompt("Iltimos, son kiriting:");
+// while (son > 100) {
+//   son = +prompt("Iltimos, 100 dan kichik son kiriting:");
+// }
+// if (son % 10 == 0 || son % 5 == 0) {
+//   console.log(son);
+// }
+
+// 9-masala
+
+do {
+    let son = prompt("Iltimos, son kiriting:");
+    if (son > 0 && son <= 10) {
+      console.log(son);
+    }
+  } while (son != 0);
+  
